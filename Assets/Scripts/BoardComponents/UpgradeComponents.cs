8347ee5b6a2402f91bf7eec88161ab9162df7224
DrewWhitmer <aswhitme@ucsc.edu>
2026-02-09 17:21:04 -0800

    component upgrading basics
    Still not entirely finished, need to get the upgrades to cost coins still.

---
+++ Assets/Scripts/BoardComponents/UpgradeComponents.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Drawing;
+using UnityEngine;
+
+public class UpgradeComponents : MonoBehaviour
+{
+    [SerializeField] private String tagToUpgrade;
+    [SerializeField] private float multiplyPointsBy;
+    [SerializeField] private int cost;
+    [SerializeField] private GameRulesManager gameRulesManager;
+    [SerializeField] private ShopUIController shopUIController;
+
+    private void EnsureRefs()
+    {
+        if (!gameRulesManager)
+        {
+            gameRulesManager = FindFirstObjectByType<GameRulesManager>();
+        }
+        if (!shopUIController)
+        {
+            shopUIController = FindFirstObjectByType<ShopUIController>();
+        }
+    }
+
+    public void UpgradeComponentByTag()
+    {
+        EnsureRefs();
+
+        if (gameRulesManager.Coins < cost)
+        {
+            return;
+        }
+
+        gameRulesManager.AddCoins(-cost);
+        shopUIController.RefreshUI();
+
+        GameObject[] objects = GameObject.FindGameObjectsWithTag(tagToUpgrade);
+        foreach (GameObject obj in objects)
+        {
+            PointAdder pointAdder = obj.GetComponent<PointAdder>();
+            MultAdder multAdder = obj.GetComponent<MultAdder>();
+            if(pointAdder)
+            {
+                pointAdder.multiplyPointsToAdd(multiplyPointsBy);
+                return;
+            }
+
+            if(multAdder)
+            {
+                multAdder.multiplyMultToAdd(multiplyPointsBy);
+            }
+        }
+    }
+}

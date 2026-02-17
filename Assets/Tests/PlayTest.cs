c6e61f7779ff2982ef6fa8af02061dd7c20791dc
DrewWhitmer <aswhitme@ucsc.edu>
2026-02-17 13:41:41 -0800

    Default Tests
    Seeing what happens with the game.ci testrunner when I push these tests. There is a test that fails in the play and edit test when I do it in the editor.

---
+++ Assets/Tests/PlayTest.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Unity.PerformanceTesting;
+using UnityEngine.SceneManagement;
+
+public class FirstTest
+{
+
+    // Tests are based on examples from: 
+    // https://docs.unity3d.com/Packages/com.unity.test-framework.performance@3.2/manual/writing-tests.html
+
+    [Test]
+    public void FirstTestSimplePasses()
+    {
+        Measure_Empty();
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator FirstTestWithEnumeratorPasses()
+    {
+        MainSceneFrameTime_StartPosition();
+        GameSceneFrameTime_StartPosition();
+        BoardSceneFrameTime_StartPosition();
+        yield return null;
+    }
+
+    [Test, Performance, Version("1")]
+    public void Measure_Empty()
+    {
+        var allocated = new SampleGroup("TotalAllocatedMemory", SampleUnit.Megabyte);
+        var reserved = new SampleGroup("TotalReservedMemory", SampleUnit.Megabyte);
+        Measure.Custom(allocated, UnityEngine.Profiling.Profiler.GetTotalAllocatedMemoryLong() / 1048576f);
+        Measure.Custom(reserved, UnityEngine.Profiling.Profiler.GetTotalReservedMemoryLong() / 1048576f);
+    }
+
+    [UnityTest, Performance, Version("4")]
+    public IEnumerator MainSceneFrameTime_StartPosition()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+
+        // Measure initial time of first 25 frames after loading the scene
+        using(Measure.Frames().Scope("FrameTime.FirstFramesAfterLoadingScene"))
+        {
+            for (var i = 0; i < 25; i++)
+            {
+                yield return null;
+            }
+        }
+
+        // Measure frame times for ten seconds during rest of the "Demo" scene
+        using (Measure.Frames().Scope("FrameTime.Main"))
+        {
+            yield return new WaitForSeconds(10);
+        }
+    }
+
+    [UnityTest, Performance, Version("4")]
+    public IEnumerator GameSceneFrameTime_StartPosition()
+    {
+        SceneManager.LoadScene("GameplayCore", LoadSceneMode.Single);
+
+        // Measure initial time of first 25 frames after loading the scene
+        using(Measure.Frames().Scope("FrameTime.FirstFramesAfterLoadingScene"))
+        {
+            for (var i = 0; i < 25; i++)
+            {
+                yield return null;
+            }
+        }
+
+        // Measure frame times for ten seconds during rest of the "Demo" scene
+        using (Measure.Frames().Scope("FrameTime.Main"))
+        {
+            yield return new WaitForSeconds(10);
+        }
+    }
+
+    [UnityTest, Performance, Version("4")]
+    public IEnumerator BoardSceneFrameTime_StartPosition()
+    {
+        SceneManager.LoadScene("Board_Alpha", LoadSceneMode.Single);
+
+        // Measure initial time of first 25 frames after loading the scene
+        using(Measure.Frames().Scope("FrameTime.FirstFramesAfterLoadingScene"))
+        {
+            for (var i = 0; i < 25; i++)
+            {
+                yield return null;
+            }
+        }
+
+        // Measure frame times for ten seconds during rest of the "Demo" scene
+        using (Measure.Frames().Scope("FrameTime.Main"))
+        {
+            yield return new WaitForSeconds(10);
+        }
+    }
+
+}

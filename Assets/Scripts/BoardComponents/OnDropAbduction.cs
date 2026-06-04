9b2127c9af181ad76a815eabc93d1157426a3200
JJ <jj.miller116@gmail.com>
2026-06-04 12:46:11 -0700

    nice

--- Assets/Scripts/BoardComponents/OnDropAbduction.cs
+++ Assets/Scripts/BoardComponents/OnDropAbduction.cs
@@ -3,22 +3,12 @@ using UnityEngine;
 public class OnDropAbduction : MonoBehaviour
 {
     [SerializeField] private DropTarget dropTarget;
-    [SerializeField] private FrenzyManager frenzyManager;
-    [SerializeField] private bool frenzyModeBeenEnabled = false;
     [SerializeField] private Abductor abductor;
     public bool gettingAbducted = false;
 
-    private void Awake()
-    {
-        frenzyManager = ServiceLocator.Get<FrenzyManager>();
-    }
-
     void Start()
     {
-        frenzyManager.OnFrenzyDeactivated += OnFrenzyEnd;
-        dropTarget.GetComponent<BoardComponent>().onBallHit += OnBallHit;
         dropTarget.OnFullyDown += StartAbduction;
-        frenzyModeBeenEnabled = false;
     }
 
     private void Update()
@@ -27,19 +17,6 @@ public class OnDropAbduction : MonoBehaviour
         {
             dropTarget.resetTimer += Time.deltaTime;
         }
-    }
-
-    void OnBallHit()
-    {
-        if (!frenzyModeBeenEnabled)
-        {
-            dropTarget.amountOfHits = 0;
-        }
-    }
-
-    void OnFrenzyEnd()
-    {
-        frenzyModeBeenEnabled = true;
     }
 
     void StartAbduction()
